20657b53bb93d3176e6044510561ecf7951d16d8
zaqw60 <zaqw60@gmail.com>
2022-10-12 12:48:44 +0300

    добавил файлы

---
+++ resources/views/account/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.account')
+@section('content')
+
+<x-app-layout>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h>Здравствуйте, {{ Auth::user()->name }}!</h>
+                    <br>
+                    @if(Auth::user()->role === '0')
+                        <a href="">В админку</a>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
+
+@endsection
+
+{{--{{ route('admin.index') }}--}}
