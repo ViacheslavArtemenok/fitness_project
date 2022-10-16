517e79a3f2e40677d4b2681a0b0f438c1b1f2d6c
Viacheslav Artemenok <vartemenok@mail.ru>
2022-10-16 21:28:06 +0300

    Вручную исправил ошибки для мержинга

--- config/pagination.php
+++ config/pagination.php
@@ -4,7 +4,9 @@ return [
     'admin' => [
         'users' => 15,
         'profiles' => 15,
-        'skills' => 15
+        'skills' => 15,
+        'tags' => 15,
+        'relations' => 15
     ],
     'trainers' => [
         'users' => 15,
