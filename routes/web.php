a186dffda46a62b8b69a24733070009a5165cf5d
Viacheslav Artemenok <vartemenok@mail.ru>
2022-10-25 12:12:31 +0300

    исправление ошибки web

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TrainerController;
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\Account\IndexController as AccountIndexController;
 use App\Http\Controllers\Account\UserController as AccountUserController;
 use \App\Http\Controllers\Account\ProfileController as AccountProfileController;
+use App\Http\Controllers\Account\SkillController as AccountSkillController;
+use \App\Http\Controllers\Account\TagController as AccountTagController;
 use App\Http\Controllers\Admin\IndexController as AdminIndexController;
 use App\Http\Controllers\Admin\ProfileController as AdminProfileController;
 use App\Http\Controllers\Admin\SkillController as AdminSkillController;
@@ -28,14 +30,6 @@ use App\Http\Controllers\SubscriptionController;
 
 Route::get('/', InfoController::class)
     ->name('info');
-//Front routes
-Route::get('/trainers/{tag_id}/{city_id}', [TrainerController::class, 'index'])
-    ->where('id', '\d+')
-    ->name('trainers.index');
-Route::get('/trainer/{id}/{city_id}', [TrainerController::class, 'show'])
-    ->where('id', '\d+')
-    ->name('trainers.show');
-Route::resource('subscriptions', SubscriptionController::class);
 
 // Account routes
 Route::middleware('auth')->group(function () {
@@ -44,11 +38,21 @@ Route::middleware('auth')->group(function () {
     Route::group(['prefix' => 'account', 'as' => 'account.'], function () {
         Route::resource('users', AccountUserController::class);
         Route::resource('profiles', AccountProfileController::class);
+        Route::resource('skills', AccountSkillController::class);
+        Route::resource('tags', AccountTagController::class);
     });
 });
 
 require __DIR__ . '/auth.php';
-//Admin routes
+
+Route::get('/trainers/{tag_id}/{city_id}', [PageController::class, 'index'])
+    ->where('id', '\d+')
+    ->name('trainers.index');
+Route::get('/trainer/{id}/{city_id}', [PageController::class, 'show'])
+    ->where('id', '\d+')
+    ->name('trainers.show');
+Route::resource('subscriptions', SubscriptionController::class);
+
 Route::middleware('auth')->group(function () {
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function () {
         Route::get('/', AdminIndexController::class)
