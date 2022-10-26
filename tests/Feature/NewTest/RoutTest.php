94f4db841b24252da2c4686a3d14727508ad82a4
Veedok <Konakov88@gmail.com>
2022-10-26 20:49:50 +0300

    Доделал Post

--- tests/Feature/NewTest/RoutTest.php
+++ tests/Feature/NewTest/RoutTest.php
@@ -2,19 +2,13 @@
 
 namespace Tests\Feature\NewTest;
 
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Models\Profile;
 use App\Models\Skill;
 use App\Models\Tag;
 use App\Models\User;
-use App\Services\UploadService;
 use Faker\Factory;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Symfony\Component\HttpFoundation\File\UploadedFile as FileUploadedFile;
 use Tests\TestCase;
 
 class test extends TestCase
@@ -50,26 +44,7 @@ class test extends TestCase
         $response = $this->actingAs($user)->get(route('account.profiles.create', $_GET));
         $response->assertOk();
     }
-    // public function test_acount_profile_controller_store(){
-    //     $user = User::factory()->create();
 
-
-    //     $_GET = ['user_id' => 14];
-
-    //     $dile = FileUploadedFile::;
-    //     $req = [
-    //     "first_name" => "123123123",
-    //     "last_name" => "123123123",
-    //     "father_name" => "123123123123",
-    //     "age" => "23",
-    //     "gender" => "male",
-    //     "image" => $file
-    // ];
-
-
-    //     $response = $this->actingAs($user)->post(route('account.profiles.store',$req , $_GET));
-    //     $response->assertOk();
-    // }
     public function test_acount_profile_controller_edit(){
         $user = User::factory()->create();
         $_GET=['profile' => 14];
@@ -81,18 +56,7 @@ class test extends TestCase
         $response = $this->actingAs($user)->get(route('account.users.index'));
         $response->assertOk();
     }
-    // public function test_acount_users_controller_store(){
-    //     $user = User::factory()->create();
-    //     $req = [
-    //     "first_name" => "123123123",
-    //     "last_name" => "123123123",
-    //     "father_name" => "123123123123",
-    //     "age" => "23",
-    //     "gender" => "male",
-    // ];
-    //     $response = $this->actingAs($user)->post(route('account.users.store', $req, $_GET));
-    //     $response->assertOk();
-    // }
+
 public function test_acount_users_controller_create(){
         $user = User::factory()->create();
         $user->role = 'IS_ADMIN';
@@ -319,5 +283,128 @@ public function test_verify_email(){
             $response->assertRedirect(route('account'));
         }
 
+
+        // Post методы
+
+
+public function test_admin_tags_controller_store(){
+        $user = User::factory()->create();
+        $user->role = 'IS_ADMIN';
+        $req = [
+            'tag' => 'Tests',
+            'created_at' => now('Europe/Moscow')
+        ];
+        $response = $this->actingAs($user)->post(route('admin.tags.store',$req , $_GET));
+        $response->assertRedirect(route('admin.tags.index'));
+    }
+    // public function test_account_users_controller_store(){
+    //     // Проблемы в контроллере Тест не проходит
+    //     $user = User::factory()->create();
+    //     $user->role = 'IS_ADMIN';
+    //     $req = [
+    //         'name'        => 'admin',
+    //         'email'       => 'admin@mail.ru',
+    //         'phone'       => '+7 (999) 999-99-99',
+    //         'role'        => User::IS_ADMIN,
+    //         'status'      => User::ACTIVE,
+    //         'password'    => Hash::make('12345678'),
+    //         'email_verified_at' => now('Europe/Moscow'),
+    //         'created_at'  => now('Europe/Moscow')
+    //     ];
+    //     $response = $this->actingAs($user)->post(route('account.users.store',$req));
+    //     $response->assertRedirect(route('account.profiles.index'));
+    // }
+    // public function test_acount_profile_controller_store(){
+    //     $user = User::factory()->create();
+    //     $_GET = ['user_id' => 14];
+    //     $img = UploadedFile::fake()->create('222222.jpg');
+    //     $req = [
+    //     "first_name" => "123123123",
+    //     "last_name" => "123123123",
+    //     "father_name" => "123123123123",
+    //     "age" => "23",
+    //     "gender" => "male",
+    //     "image" => $img
+    // ];
+    //     $response = $this->actingAs($user)->post(route('account.profiles.store',$req , $_GET));
+    //     $response->assertOk();
+    // }
+public function test_admin_profile_controller_store(){
+        $user = User::factory()->create();
+        $_GET = ['user_id' => 14];
+        $user->role = 'IS_ADMIN';
+        $faker = Factory::create();
+        $req = [
+            'user_id' => 99,
+            'first_name' => $faker->firstNameFemale(),
+            'last_name' => $faker->lastName('female'),
+            'father_name' => 'Петровна',
+            'age' => rand(25, 45),
+            'gender' => 'female',
+            'image' => 'https://cdn.inskill.ru/media/32540/c/1591358903_o4XapEybYWOG87t8-thumb.jpg?v=1591358908',
+            'created_at' => now('Europe/Moscow'),
+    ];
+        $response = $this->actingAs($user)->post(route('admin.profiles.store',$req , $_GET));
+        $response->assertRedirect(route('admin.profiles.index'));
+    }
+//     public function test_admin_relations_controller_store(){
+// // Не написана функция в самом контроллере
+//         $user = User::factory()->create();
+//         $_GET = ['user_id' => 14];
+//         $user->role = 'IS_ADMIN';
+//         $req = [
+//             'user_id' => $user->id,
+//             'tag_id' => rand(1, 22),
+//             'created_at' => now('Europe/Moscow')
+//     ];
+//         $response = $this->actingAs($user)->post(route('admin.relations.store',$req , $_GET));
+//         $response->assertRedirect(route('admin.relations.index'));
+//     }
+
+public function test_admin_skills_controller_store(){
+        $user = User::factory()->create();
+        $_GET = ['user_id' => 14];
+        $faker = Factory::create();
+        $user->role = 'IS_ADMIN';
+        $req = [
+            'user_id'         => $user->id,
+            'location'        => 'Москва',
+            'education'       => $faker->company(),
+            'experience'      => rand(1, 20),
+            'achievements'    => $faker->paragraph(rand(3, 8)),
+            'skills_list'     => $faker->paragraph(rand(6, 10)),
+            'description'     => $faker->paragraph(rand(6, 10)),
+            'created_at'      => now('Europe/Moscow')
+    ];
+        $response = $this->actingAs($user)->post(route('admin.skills.store',$req , $_GET));
+        $response->assertRedirect(route('admin.skills.index'));
+    }
+// public function test_admin_user_controller_store(){
+//     // Проблемы в контроллере Тест не проходит
+//         $user = User::factory()->create();
+//         $_GET = ['user_id' => 14];
+//         $faker = Factory::create();
+//         $user->role = 'IS_ADMIN';
+//         $req = [
+//             'name'        => 'admin',
+//             'email'       => 'admin@mail.ru',
+//             'phone'       => '+7 (999) 999-99-99',
+//             'role'        => User::IS_ADMIN,
+//             'status'      => User::ACTIVE,
+//             'password'    => Hash::make('12345678'),
+//             'email_verified_at' => now('Europe/Moscow'),
+//             'created_at'  => now('Europe/Moscow')
+//     ];
+//         $response = $this->actingAs($user)->post(route('admin.users.store',$req , $_GET));
+//         $response->assertRedirect(route('admin.users.store'));
+//     }
+public function test_account_profiles_controller_destroy(){
+        $user = User::factory()->create();
+        $profile = Profile::latest()->take(1)->get();
+        $_GET = ['profile' => $profile];
+        $profile->deleted_at = now('Europe/Moscow');
+        $response = $this->actingAs($user)->delete(route('account.profiles.destroy',$profile, $_GET));
+        $response->assertOk();
+    }
 
 }
