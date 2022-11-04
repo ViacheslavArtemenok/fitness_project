f2be0aa455f02c294e33674d16195dfa596f9a38
Viacheslav Artemenok <vartemenok@mail.ru>
2022-11-04 23:46:37 +0300

    Добавил изменения из dev

--- app/Http/Controllers/Admin/CharacteristicController.php
+++ app/Http/Controllers/Admin/CharacteristicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Characteristic;
 
+use App\Http\Requests\Characteristics\EditRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -18,7 +19,9 @@ class CharacteristicController extends Controller
      */
     public function index()
     {
-        $characteristics = Characteristic::query()->paginate(config('pagination.admin.characteristics'));
+        $characteristics = Characteristic::query()
+            ->with('profile')
+            ->paginate(config('pagination.admin.characteristics'));
 
         return view('admin.characteristics.index', [
             'characteristics' => $characteristics
@@ -74,22 +77,20 @@ class CharacteristicController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
+     * @param  EditRequest  $request
      * @param  Characteristic $characteristic
      * @return RedirectResponse
      */
-    public function update(Request $request, Characteristic $characteristic): RedirectResponse
+    public function update(EditRequest $request, Characteristic $characteristic): RedirectResponse
     {
-//        $user = $user->fill(array_merge($request->validated(),
-//            ['password' => Hash::make($request['password'])]
-//        ));
-//
-//        if($user->save()) {
+        $characteristic = $characteristic->fill($request->validated());
+
+        if ($characteristic->save()) {
             return redirect()->route('admin.characteristics.index')
                 ->with('success',  __('messages.admin.characteristics.update.success'));
-//        }
-//
-//        return back()->with('error', __('messages.admin.users.update.fail'));
+        }
+
+        return back()->with('error', __('messages.admin.characteristics.update.fail'));
     }
 
     /**
